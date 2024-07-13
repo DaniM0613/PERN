c7e17aef8464aa0db2ff903005632e90ae6ba1b5
DaniM0613 <wwh.rj.92@gmail.com>
2024-07-13 17:45:43 -0600

    agregando un boton para editar usando LINK de react-router-dom

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,4 +1,5 @@
-import type { Product } from "../types"
+import { Link, useNavigate }  from 'react-router-dom'
+import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
 
@@ -23,7 +24,10 @@ export default function ProductDetails({product} : ProductDetailsProps) {
         </td>
         <td className="p-3 text-lg text-gray-800">
           <div className="flex gap-2 items-center">
-            <button>Editar</button> 
+            <Link
+              to={`/productos/${product.id}/editar`}
+              className='bg-indigo-600 text-center text-white rounded-lg  w-full p-2 uppercase font-bold text-xs'
+            >Editar</Link> 
           </div>
         </td>
     </tr>
