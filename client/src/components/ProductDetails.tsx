1a502b1a3a6b3e607640d0b01d2ee1ed88c10eb0
DaniM0613 <wwh.rj.92@gmail.com>
2024-07-13 17:54:13 -0600

    redireccionar con useNavigate

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate }  from 'react-router-dom'
+import { useNavigate }  from 'react-router-dom'
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -9,7 +9,8 @@ type ProductDetailsProps = {
 
 export default function ProductDetails({product} : ProductDetailsProps) {
 
-    const isAvailable = product.availability
+   const navigate = useNavigate()
+   const isAvailable = product.availability
 
   return (
     <tr className="border-b">
@@ -24,10 +25,10 @@ export default function ProductDetails({product} : ProductDetailsProps) {
         </td>
         <td className="p-3 text-lg text-gray-800">
           <div className="flex gap-2 items-center">
-            <Link
-              to={`/productos/${product.id}/editar`}
+            <button
+              onClick={() => navigate(`/productos/${product.id}/editar`)}
               className='bg-indigo-600 text-center text-white rounded-lg  w-full p-2 uppercase font-bold text-xs'
-            >Editar</Link> 
+            >Editar</button> 
           </div>
         </td>
     </tr>
