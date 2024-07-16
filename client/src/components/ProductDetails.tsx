9dcde6c77a53ee3c5574affeb046fddc2609cd8b
DaniM0613 <wwh.rj.92@gmail.com>
2024-07-15 22:40:28 -0600

    Habilitando Editar desde la URL

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -26,11 +26,7 @@ export default function ProductDetails({product} : ProductDetailsProps) {
         <td className="p-3 text-lg text-gray-800">
           <div className="flex gap-2 items-center">
             <button
-              onClick={() => navigate(`/productos/${product.id}/editar`, {
-                 state: {
-                   product
-                 }
-              })}
+              onClick={() => navigate(`/productos/${product.id}/editar`)}
               className='bg-indigo-600 text-center text-white rounded-lg  w-full p-2 uppercase font-bold text-xs'
             >Editar</button> 
           </div>
--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -1,6 +1,12 @@
-import { Link, Form, useActionData, ActionFunctionArgs, redirect, useLocation } from 'react-router-dom'
+import { Link, Form, useActionData, ActionFunctionArgs, redirect, LoaderFunctionArgs } from 'react-router-dom'
 import ErrorMessage from '../components/ErrorMessage'
 import {addProduct} from '../services/ProductService'
+
+export async function loader({params} : LoaderFunctionArgs){
+  console.log(params.id)
+
+  return {}
+}
 
 export async function action({request} : ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData())
@@ -19,10 +25,7 @@ export async function action({request} : ActionFunctionArgs) {
 
 export default function EditProduct() {
     const error = useActionData() as string
-    const {state} = useLocation()
-
-    console.log(state)
-
+   
   return (
     <>
      <div className='flex justify-between'>
@@ -53,7 +56,7 @@ export default function EditProduct() {
            className='mt-2 block w-full p-3 bg-gray-50'
            placeholder='Nombre del Producto'
            name='name'
-           defaultValue={state.product.name}
+           
         />
        </div>
        <div className='mb-4'>
@@ -67,7 +70,7 @@ export default function EditProduct() {
            className='mt-2 block w-full p-3 bg-gray-50'
            placeholder='Precio Producto. ej. 200, 300'
            name='price'
-           defaultValue={state.product.price}
+          
          />
         </div>
          <input
